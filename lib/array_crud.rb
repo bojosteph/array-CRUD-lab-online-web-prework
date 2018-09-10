bcaf51e3d7b34512546ed6f294c5d0d4b604261c
Roberto Samia <bojosteph@gmail.com>
2018-09-10 21:19:33 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  numbers = [1,2,3,4,5,]
 end
 
 def retrieve_last_element_from_array(array)
