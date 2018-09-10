0890c5384d7113cb56c5c1ac67ed1701f5c70822
Roberto Samia <bojosteph@gmail.com>
2018-09-10 21:18:17 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ def add_element_to_end_of_array("numbers")
 end
 
 def add_element_to_start_of_array("numbers")
-  numbers.unshift
+  numbers.unshift(0)
 end
 
 def remove_element_from_end_of_array(array)
