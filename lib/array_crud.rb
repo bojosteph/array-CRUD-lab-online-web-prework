a567f9ab77a6dc52cbb096ae12190a1f880ec664
Roberto Samia <bojosteph@gmail.com>
2018-09-10 21:19:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  numbers = [1,2,3,4,5,]
 end
 
 def retrieve_element_from_index(array, index_number)
